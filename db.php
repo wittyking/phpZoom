1b6cea0709be5ba0673a284e324cfb397b0ec739
wittyking <32215818+wittyking@users.noreply.github.com>
2026-03-22 16:20:24 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host     = getenv('DB_HOST') ?: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$host     = getenv('DB_HOST') ?: 'ep-morning-night-a15qw5d1-pooler.ap-southeast-1.aws.neon.tech';
 $port     = getenv('DB_PORT') ?: '5432';
-$dbname   = getenv('DB_NAME') ?: 'xxxxxxxxxxxx';
-$username = getenv('DB_USER') ?: 'xxxxxxxxxxxx';
-$password = getenv('DB_PASSWORD') ?: 'xxxxxxxxxxxxx';
+$dbname   = getenv('DB_NAME') ?: 'neondb';
+$username = getenv('DB_USER') ?: 'neondb_owner';
+$password = getenv('DB_PASSWORD') ?: 'npg_cRja35mYHIth';
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
